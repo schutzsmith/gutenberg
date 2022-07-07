185a8b495e7f6e0fc420499b693c713f1fcd6c44
David Arenas <david.arenas@automattic.com>
2022-07-07 15:03:05 +0200

    Merge the blocks and remove the old one

--- packages/block-library/src/comments/edit/template.js
+++ packages/block-library/src/comments/edit/template.js
@@ -1,13 +1,3 @@
-/**
- * WordPress dependencies
- */
-import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
-
-/**
- * Internal dependencies
- */
-import CommentsInspectorControls from './edit/comments-inspector-controls';
-
 const TEMPLATE = [
 	[ 'core/comments-title' ],
 	[
@@ -88,24 +78,4 @@ const TEMPLATE = [
 	[ 'core/post-comments-form' ],
 ];
 
-export default function CommentsEdit( { attributes, setAttributes } ) {
-	const { tagName: TagName } = attributes;
-
-	const blockProps = useBlockProps( {
-		// We add the previous block name for backward compatibility.
-		className: 'wp-block-comments-query-loop',
-	} );
-	const innerBlocksProps = useInnerBlocksProps( blockProps, {
-		template: TEMPLATE,
-	} );
-
-	return (
-		<>
-			<CommentsInspectorControls
-				attributes={ attributes }
-				setAttributes={ setAttributes }
-			/>
-			<TagName { ...innerBlocksProps } />
-		</>
-	);
-}
+export default TEMPLATE;
